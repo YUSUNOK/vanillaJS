fb79c284d6dc0eeedf2d41c35b39ff468778c9a5
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2022-12-23 02:13:57 +0900

    3.0 이전 / 내일까지 다 수강하자...

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-function sayHello(name){
-    console.log("hello, my name is " + name);
+while (true) {
+  const age = parseInt(prompt("how old are you?"));
+  console.log(typeof age);              
+  console.log(age);    
+  if (isNaN(age)) {
+    alert("잘못입력했습니다. 다시 입력하세요");
+  } else {
+    if (age >= 20) {
+      console.log("돈 여기 있습니다.");
+    } else {
+      console.log("술을 살 수 없어요 바바이 ~ ");
+    }
+    break;
+  }
 }
-
-sayHello("유선옥");
