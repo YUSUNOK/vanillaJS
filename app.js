0acb434898bcaf65ba1a3d0cfc6928be48c59fd3
Doyun Lee <sunokryu319@gmail.com>
2022-12-23 11:59:53 +0900

    23일 점심 12:00까지 3.1까지 수강

--- app.js
+++ app.js
@@ -1,15 +1,2 @@
-while (true) {
-  const age = parseInt(prompt("how old are you?"));
-  console.log(typeof age);              
-  console.log(age);    
-  if (isNaN(age)) {
-    alert("잘못입력했습니다. 다시 입력하세요");
-  } else {
-    if (age >= 20) {
-      console.log("돈 여기 있습니다.");
-    } else {
-      console.log("술을 살 수 없어요 바바이 ~ ");
-    }
-    break;
-  }
-}
+const result = document.querySelectorAll(".hello h1");
+console.log(result);
