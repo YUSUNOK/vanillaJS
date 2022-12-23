11d1cb5fcb8fd2a4f7fde5ae56b80f13a61d24a4
Doyun Lee <sunokryu319@gmail.com>
2022-12-23 23:28:50 +0900

    12/23 js

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-const result = document.querySelectorAll(".hello h1");
-console.log(result);
+const result = document.querySelector('#first h1:first-child');
+console.dir(result);
+result.addEventListener("click", function whenClick(){
+  if(result.style.color != ''){
+    result.style.color = "";
+  }
+  else{
+    result.style.color = "blue";
+  }
+});
