e74cfbc985d590ffe832620b7ad57921c0c77485
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2022-12-25 17:56:19 +0900

    12/25

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-const result = document.querySelector('#first h1:first-child');
+const result = document.querySelector("#first h1:first-child");
 console.dir(result);
-result.addEventListener("click", function whenClick(){
-  if(result.style.color != ''){
+result.addEventListener("click", function whenClick() {
+  if (result.style.color != "") {
     result.style.color = "";
-  }
-  else{
+  } else {
     result.style.color = "blue";
   }
+});
+// result.addEventListener("mouseenter", function whenMouseEnter() {
+//   result.innerText = "Mouse Enter!";
+// });
+function whenMouseEnter() {
+  result.innerText = "Mouse Enter!";
+}
+result.onmouseenter = whenMouseEnter;
+result.addEventListener("mouseleave", function whenMouseLeave() {
+  result.innerText = "Mouse Leave!";
+});
+
+window.addEventListener("resize", function windowResize() {
+  this.document.body.style.backgroundColor = "red";
 });
